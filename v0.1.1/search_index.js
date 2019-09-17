6b095dd07d170f296ac17b381ccab78a898ce681
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-09-17 09:19:19 +0000

    build based on 097660f

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ImageNoise-1","page":"ImageNoise","title":"ImageNoise","text":"","category":"section"},{"location":"#","page":"ImageNoise","title":"ImageNoise","text":"This is a package for work related to image noise","category":"page"},{"location":"#","page":"ImageNoise","title":"ImageNoise","text":"and it's at its early stage","category":"page"}]
+}
